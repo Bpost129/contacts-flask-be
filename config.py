a9bfa29b27cae6e3838f5b420b8e98c78ccc464c
Bpost129 <Bpost129@yahoo.com>
2025-01-14 21:57:56 -0500

    complete config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_sqlaclchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contactsdb.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contactsfr.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
