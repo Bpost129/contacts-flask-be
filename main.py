2e77c29d7b98612adbfe23c1921d0212b19faca3
Bpost129 <Bpost129@yahoo.com>
2025-01-15 23:36:10 -0500

    clean code

--- main.py
+++ main.py
@@ -57,10 +57,11 @@ def delete_contact(user_id):
 
   return jsonify({"message": "User deleted!"}), 200
 
+
+
 if __name__ == "__main__":
   with app.app_context():
     db.create_all()
   
   app.run(debug=True)
-
-
+  
